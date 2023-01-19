6179b606f422f6c1f1c7df3ec917825cb298b0b4
Sujan Khanal <sujankhanal.photo@gmail.com>
2023-01-19 16:41:39 +0545

    PPP

---
+++ switch.cs
@@ -0,0 +1,33 @@
+using System;
+public struct Student{
+   public string name;
+   public int rollno;
+   public string address;
+
+}
+public class Program{
+    public static void Main(){
+    
+    
+Student s1;
+Student s2;
+Student s3;
+
+s1.name="Sushil";
+s1.address="Ghar bata";
+s1.rollno=1;
+
+s2.name="Sujan";
+s2.address="Sishuwa";
+s2.rollno=21;
+
+s3.name="Messi";
+s3.address="Argentina";
+s3.rollno=10;
+Console.WriteLine("Name:"+" "+s1.name+" "+", address:"+" "+s1.address+" "+", rollno:"+" "+s1.rollno);
+Console.WriteLine("Name:"+" "+s2.name+" "+", address:"+" "+s2.address+" "+", rollno:"+" "+s2.rollno);
+Console.WriteLine("Name:"+" "+s3.name+" "+", address:"+" "+s3.address+" "+", rollno:"+" "+s3.rollno);
+
+}
+}
+ 
