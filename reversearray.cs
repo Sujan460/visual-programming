1324c754988c5c1d71a4b4e2b92d08be0c0b8333
Sujan Khanal <110553592+Sujan460@users.noreply.github.com>
2023-02-12 19:44:36 +0545

    Reverse of array

---
+++ reversearray.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Hello{
+    class PPP{
+        static void Main(string[]args){
+            int[]arr=new int[]{1,2,3};
+            Array.Reverse(arr);
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+                {
+                    
+                }
+            }
+        }
+    }
+}
