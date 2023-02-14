342ae8209938f943a29a15f09bee55421864cf79
Sujan Khanal <110553592+Sujan460@users.noreply.github.com>
2023-02-14 21:30:11 +0545

    ..................................................

--- Program.cs
+++ Program.cs
@@ -1,16 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-namespace HelloWorld{
-    public class Sum{
-        static void Main(string[]args){
-            int a = 3;
-            int b = 5;
-            int temp;
-            Console.WriteLine("Before Swapping the value of a={0},b={1}",a,b);
-            temp=a;
-            a=b;
-            b=temp;
-            Console.WriteLine("After Swapping The value of a={0},b={1}",a,b);
+﻿using System;
+class Program{
+    static void Main(string[]args){
+        unsafe{
+            string a = "Sujan Khanal";
+            
         }
     }
 }
